9dedfae045b41808f96fb5fd10ac7c96f36c704e
Ilkeun Chung <ilkeun.chung@gmail.com>
2022-02-04 19:22:28 +0900

    Linux2

---
+++ auth.py
@@ -0,0 +1,8 @@
+ilkeun.chung@gmail.com
+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
+
+
+git config --global credential.helper cache
+git commit -am "Linux1"
+git push origin main
+git pull origin main
